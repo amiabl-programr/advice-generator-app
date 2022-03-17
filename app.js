d3614a18c3f65067d08f74d74fb8aef6f1442162
AyoOlu1 <oluwayemivictor15@gmail.com>
2022-03-17 05:42:32 +0100

    fetched data from api

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-        
+var advice_id = document.querySelector(".advice__id");
+var advice_content = document.querySelector(".advice__content");
+
+function getAdvice() {
+  const URL = "https://api.adviceslip.com/advice";
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      let output_id = "";
+      let output_content = "";
+      let data_array = Object.entries(data); // flexiple.com/loop-through-object-javascript
+      // console.log(data_array[0][1].advice);
+      data_array.forEach((advice) => {
+        // console.log(advice[1].id);
+        // console.log(advice[1].advice);
+        output_id += advice[1].id;
+        output_content += advice[1].advice;
+      });
+      advice_id.innerHTML = output_id;
+      advice_content.innerHTML = output_content;
+    });
+}
+
+getAdvice();
